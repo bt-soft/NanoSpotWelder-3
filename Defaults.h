92b0a3d9b77776b80cf89a7a74252b4c84e7ec17
bt-soft <bt-soft@freemail.hu>
2019-01-16 19:04:05 +0100

    - konfig tárolás

--- Defaults.h
+++ Defaults.h
@@ -24,19 +24,19 @@
 #define DEFAULTS_H_
 
 #define DEF_LCD_CONTRAST				30 		/* LCD kontraszt */
-#define DEF_LCD_BIAS					4 		/* LCD elõfeszítés */
+#define DEF_LCD_BIAS					4		/* LCD elõfeszítés */
 
 #define DEF_PULSE_COUNT_WELD_MODE		true	/* pulzusszámlálásos hegesztési mód */
 #define DEF_BEEP_STATE					true  	/* Beep */
 #define DEF_BACKLIGHT_STATE				true 	/* LCD háttérvilágítás */
 
 ///  1 periódus: 100Hz -> 10ms
-#define DEF_PREWELD_PULSE_CNT 			2		/* 20ms */
-#define DEF_PAUSE_PULSE_CNT 			5	 	/* 50ms */
-#define DEF_WELD_PULSE_CNT 				4 		/* 40ms */
+#define DEF_PREWELD_PULSE_CNT 			6
+#define DEF_PAUSE_PULSE_CNT 			4
+#define DEF_WELD_PULSE_CNT 				5
 
-#define DEF_PACKET_CNT					1		/* hegesztés csomagok ismétlési száma */
-#define DEF_PACKET_PAUSE_CNT			10      /* hegesztés csomagok ismétlése közötti várakozási impulzusok száma */
+#define DEF_PACKET_CNT					2		/* hegesztés csomagok ismétlési száma */
+#define DEF_PACKET_PAUSE_CNT			5       /* hegesztés csomagok ismétlése közötti várakozási impulzusok száma */
 
 
 #define DEF_MOT_TEMP_ALARM 				40 		/* MOT hõmérséklet riasztás bekapcsolási határ °C-ban */
